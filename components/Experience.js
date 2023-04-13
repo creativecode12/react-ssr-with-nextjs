5bdee3b8cc587e1a56b9a9076953665bec8ef123
creativecode12 <88997895+creativecode12@users.noreply.github.com>
2023-04-13 23:23:21 +0530

    basic pages added

--- components/Experience.js
+++ components/Experience.js
@@ -4,7 +4,7 @@ export default function Experience() {
   return (
     <section className="bg-white dark:bg-gray-800">
       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-
+        Experience
       </div>
       <div className="bg-[#F1F1F1] dark:bg-gray-900 -mt-4">
 
